88661d76d5c5495bcb09d9913780499221afd648
Shora Senfi <shora@fsug.sharif.edu>
2016-12-06 22:54:08 +0330

    raise value error when secrets could not be loaded

--- settings.py
+++ settings.py
@@ -4,3 +4,8 @@ TOKEN = os.environ.get('SHORA_TELEGRAM_KEY', '')
 SIGNING_SECRET = os.environ.get('SHORA_SIGN_SECRET', '')
 SHORA_CALLBACK_URL = 'http://shora.ce.sharif.edu/issues/ajax/bot/add'
 SLEEP_TIME = 10
+
+
+if not TOKEN or not SIGNING_SECRET:
+   raise ValueError('Couldn\'t load environment variables')
+
