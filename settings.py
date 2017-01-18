1cdbbab8b0a19cda88510d359137e999c67bc366
MohammadJafar MashhadiEbrahim <MJafarMashhadi@users.noreply.github.com>
2017-01-18 16:27:24 +0330

    Updated logging

--- settings.py
+++ settings.py
@@ -1,7 +1,14 @@
 import os
+from settings import DSN_ADDRESS
+from raven.handlers.logging import SentryHandler
+from raven import Client
+from raven.conf import setup_logging
+
 
 TOKEN = os.environ.get('SHORA_TELEGRAM_KEY', '')
 SIGNING_SECRET = os.environ.get('SHORA_SIGN_SECRET', '')
 SHORA_CALLBACK_URL = 'http://shora.ce.sharif.edu/issues/ajax/bot/add'
 SLEEP_TIME = 10
 DSN_ADDRESS = '/home/shora/config/bot_dsn.conf'
+
+setup_logging(SentryHandler(Client(open(DSN_ADDRESS).read().replace('\n', ''))))
